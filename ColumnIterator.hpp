280bb72420413645680bf66ed4bc1fc9cfab56c0
Andrea Bertoldo <andrea.bertoldo.btf@outlook.com>
2019-05-24 21:29:36 +0200

    add .clang-format

--- ColumnIterator.hpp
+++ ColumnIterator.hpp
@@ -5,115 +5,129 @@
 
 namespace BbMath
 {
-	//========================================================================================//
-	//================================= CLASS ColumnIterator =================================//
-	//========================================================================================//
-	template<typename T>
-	class ColumnIterator
-		: public std::iterator<std::random_access_iterator_tag, T> // Inherit typedefs
+//========================================================================================//
+//================================= CLASS ColumnIterator =================================//
+//========================================================================================//
+template <typename T>
+class ColumnIterator
+	: public std::iterator<std::random_access_iterator_tag, T> // Inherit typedefs
+{
+public:
+	typedef std::size_t size_type;
+	typedef std::ptrdiff_t difference_type;
+
+private:
+	T** mat;		  // Points to the matrix over which the iterator will operate
+	size_type row;	// This is the index that is incremented/decremented during iterations
+	size_type column; // Should never change as the iterator iterates over the rows of a given column
+
+public:
+	// Constructors
+	ColumnIterator(T** p, const size_type& i, const size_type& j)
+		: mat(p), row(i), column(j) {}
+	ColumnIterator(const ColumnIterator& other)
+		: mat(other.mat), row(other.row), column(other.column) {}
+	ColumnIterator(ColumnIterator&& other)
+		: mat(other.mat), row(other.row), column(other.column) { other.mat = nullptr; }
+	~ColumnIterator() {}
+
+	ColumnIterator& operator=(const ColumnIterator& rhs)
 	{
-	public:
-		typedef std::size_t	   size_type;
-		typedef std::ptrdiff_t difference_type;
-
-	private:
-		T** mat;	  // Points to the matrix over which the iterator will operate
-		size_type row;	  // This is the index that is incremented/decremented during iterations
-		size_type column; // Should never change as the iterator iterates over the rows of a given column
-
-	public:
-		// Constructors
-		ColumnIterator(T** p, const size_type& i, const size_type& j)
-			: mat(p), row(i), column(j) { }
-		ColumnIterator(const ColumnIterator& other)
-			: mat(other.mat), row(other.row), column(other.column) { }
-		ColumnIterator(ColumnIterator&& other)
-			: mat(other.mat), row(other.row), column(other.column) { other.mat = nullptr; }
-		~ColumnIterator() { }
-
-		ColumnIterator& operator=(const ColumnIterator& rhs)
-			{ if (*this != rhs) { mat = rhs.mat; column = rhs.column; row = rhs.row; } return *this; }
-		ColumnIterator& operator=(ColumnIterator&& rhs)
-			{ if (*this != rhs) { mat = rhs.mat; column = rhs.column; row = rhs.row; rhs.mat = nullptr; } return *this; }
-
-		// Access operators
-			  T& operator*()				   { return mat[row][column]; }
-//		const T& operator*() const			   { return mat[row][column]; }
-			  T& operator[](size_type i)	   { return mat[i][column]; }
-//		const T& operator[](size_type i) const { return mat[i][column]; }
-
-		// Increment/decrement operators
-		ColumnIterator& operator++() { ++row; return *this; }
-		ColumnIterator& operator--() { --row; return *this; }
-		ColumnIterator operator+(const size_type& n)   const { return ColumnIterator(mat, row + n, column); }
-		ColumnIterator operator-(const size_type& n)   const { return ColumnIterator(mat, row - n, column); }
-		size_type operator-(const ColumnIterator& rhs) const { return row - rhs.row; }
-		template<typename C>
-		friend ColumnIterator<C> operator+(size_type n, const ColumnIterator<C>& rhs);
-
-		// Comparison operators
-		bool operator==(const ColumnIterator& rhs) const { return mat == rhs.mat && column == rhs.column && row == rhs.row; }
-		bool operator!=(const ColumnIterator& rhs) const { return !(*this == rhs); }
-		bool operator< (const ColumnIterator& rhs) const;
-		bool operator<=(const ColumnIterator& rhs) const;
-		bool operator> (const ColumnIterator& rhs) const;
-		bool operator>=(const ColumnIterator& rhs) const;
-	};
-
-	template<typename T>
-	inline
-	ColumnIterator<T> operator+(std::size_t n, const ColumnIterator<T>& rhs)
-	{
-		return rhs + n;
-	}
-
-	template<typename T>
-	inline
-	bool ColumnIterator<T>::operator< (const ColumnIterator<T>& rhs) const
-	{
-		if (mat != rhs.mat || column != rhs.column)
-		{
-			return false;
+		if (*this != rhs) {
+			mat = rhs.mat;
+			column = rhs.column;
+			row = rhs.row;
 		}
-
-		return row < rhs.row;
+		return *this;
 	}
-
-	template<typename T>
-	inline
-	bool ColumnIterator<T>::operator<=(const ColumnIterator<T>& rhs) const
+	ColumnIterator& operator=(ColumnIterator&& rhs)
 	{
-		if (mat != rhs.mat || column != rhs.column)
-		{
-			return false;
+		if (*this != rhs) {
+			mat = rhs.mat;
+			column = rhs.column;
+			row = rhs.row;
+			rhs.mat = nullptr;
 		}
-
-		return row <= rhs.row;
+		return *this;
 	}
 
-	template<typename T>
-	inline
-	bool ColumnIterator<T>::operator> (const ColumnIterator<T>& rhs) const
+	// Access operators
+	T& operator*() { return mat[row][column]; }
+	//		const T& operator*() const			   { return mat[row][column]; }
+	T& operator[](size_type i) { return mat[i][column]; }
+	//		const T& operator[](size_type i) const { return mat[i][column]; }
+
+	// Increment/decrement operators
+	ColumnIterator& operator++()
 	{
-		if (mat != rhs.mat || column != rhs.column)
-		{
-			return false;
-		}
-
-		return row > rhs.row;
+		++row;
+		return *this;
 	}
-
-	template<typename T>
-	inline
-	bool ColumnIterator<T>::operator>=(const ColumnIterator<T>& rhs) const
+	ColumnIterator& operator--()
 	{
-		if (mat != rhs.mat || column != rhs.column)
-		{
-			return false;
-		}
-
-		return row >= rhs.row;
+		--row;
+		return *this;
 	}
+	ColumnIterator operator+(const size_type& n) const { return ColumnIterator(mat, row + n, column); }
+	ColumnIterator operator-(const size_type& n) const { return ColumnIterator(mat, row - n, column); }
+	size_type operator-(const ColumnIterator& rhs) const { return row - rhs.row; }
+	template <typename C>
+	friend ColumnIterator<C> operator+(size_type n, const ColumnIterator<C>& rhs);
+
+	// Comparison operators
+	bool operator==(const ColumnIterator& rhs) const { return mat == rhs.mat && column == rhs.column && row == rhs.row; }
+	bool operator!=(const ColumnIterator& rhs) const { return !(*this == rhs); }
+	bool operator<(const ColumnIterator& rhs) const;
+	bool operator<=(const ColumnIterator& rhs) const;
+	bool operator>(const ColumnIterator& rhs) const;
+	bool operator>=(const ColumnIterator& rhs) const;
+};
+
+template <typename T>
+inline ColumnIterator<T> operator+(std::size_t n, const ColumnIterator<T>& rhs)
+{
+	return rhs + n;
 }
+
+template <typename T>
+inline bool ColumnIterator<T>::operator<(const ColumnIterator<T>& rhs) const
+{
+	if (mat != rhs.mat || column != rhs.column) {
+		return false;
+	}
+
+	return row < rhs.row;
+}
+
+template <typename T>
+inline bool ColumnIterator<T>::operator<=(const ColumnIterator<T>& rhs) const
+{
+	if (mat != rhs.mat || column != rhs.column) {
+		return false;
+	}
+
+	return row <= rhs.row;
+}
+
+template <typename T>
+inline bool ColumnIterator<T>::operator>(const ColumnIterator<T>& rhs) const
+{
+	if (mat != rhs.mat || column != rhs.column) {
+		return false;
+	}
+
+	return row > rhs.row;
+}
+
+template <typename T>
+inline bool ColumnIterator<T>::operator>=(const ColumnIterator<T>& rhs) const
+{
+	if (mat != rhs.mat || column != rhs.column) {
+		return false;
+	}
+
+	return row >= rhs.row;
+}
+} // namespace BbMath
 
 #endif // GUARD_COLUMNITERATOR_HPP
--- main.cpp
+++ main.cpp
@@ -1,12 +1,12 @@
-#include "BbVector.hpp"
 #include "BbMatrix.hpp"
-#include <iostream>
-#include <vector>
-#include <chrono>
-#include <string>
+#include "BbVector.hpp"
 #include <algorithm>
-#include <random>
+#include <chrono>
 #include <complex>
+#include <iostream>
+#include <random>
+#include <string>
+#include <vector>
 
 using namespace std;
 using namespace BbMath;
@@ -23,25 +23,24 @@ int main(int argc, char* argv[])
 	// Stopwatch used for the tests
 	BbStopWatch sw(BbStopWatch::TimeUOM::milliseconds);
 
-	try
-	{
+	try {
 		{
-			vector<double> vec{ 1., 2., 3., -55. };
+			vector<double> vec{1., 2., 3., -55.};
 			cout << BbVectorD(vec).get_sum_abs() << endl;
 
-			BbVectorD bbv{ 1., 5. };
+			BbVectorD bbv{1., 5.};
 			cout << bbv << endl;
 
-			BbVectorD a{ 2., 3., 5. };
-			cout << "BbVector a, constructed with an initializer_list: " << endl << a << endl;
+			BbVectorD a{2., 3., 5.};
+			cout << "BbVector a, constructed with an initializer_list: " << endl
+				 << a << endl;
 
-			BbMatrixD A
-			{
-				{ 1., 3., 4. },
-				{ 2., 7., 9. },
-				{ -11., 45., 67. }
-			};
-			cout << "BbMatrix A, constructed from initializer_lists: " << endl << A << endl;
+			BbMatrixD A{
+				{1., 3., 4.},
+				{2., 7., 9.},
+				{-11., 45., 67.}};
+			cout << "BbMatrix A, constructed from initializer_lists: " << endl
+				 << A << endl;
 		}
 
 		{
@@ -52,7 +51,7 @@ int main(int argc, char* argv[])
 			a.push_back(-666.);
 			a.push_back(1.E+4);
 			a.push_back(.1111);
-/*
+			/*
 			cout << "a = " << a << endl;
 			cout << "a.get_min() = " << a.get_min() << endl;
 			cout << "a.get_min_abs() = " << a.get_min_abs() << endl;
@@ -85,8 +84,7 @@ int main(int argc, char* argv[])
 
 			sw.restart();
 			BbVectorD c;
-			for (i = 0; i < 10000000; ++i)
-			{
+			for (i = 0; i < 10000000; ++i) {
 				c = A.get_column(6);
 				c[6] = 667.;
 			}
@@ -96,8 +94,7 @@ int main(int argc, char* argv[])
 
 			sw.restart();
 			BbVectorD r;
-			for (i = 0; i < 10000000; ++i)
-			{
+			for (i = 0; i < 10000000; ++i) {
 				r = A.get_row(6);
 				r[6] = 667.;
 			}
@@ -107,7 +104,7 @@ int main(int argc, char* argv[])
 		}
 
 		{
-			double list[5] = { 1., -5., 4., 53., -0.66 };
+			double list[5] = {1., -5., 4., 53., -0.66};
 			vector<double> vec(5000000, 6.);
 
 			cout << "Initialization:" << endl;
@@ -119,9 +116,9 @@ int main(int argc, char* argv[])
 			cout << "Norms" << endl;
 
 			cout << "Assignment:" << endl;
-			bbV  = 666.;
+			bbV = 666.;
 
-			bbA  = bbV;
+			bbA = bbV;
 
 			cout << bbV << endl;
 			cout << bbA << endl;
@@ -135,17 +132,20 @@ int main(int argc, char* argv[])
 			BbMatrixD B(5, 7), C(3, 4, 2.);
 			B = 666.;
 
-			cout << "Matrix B =" << endl << B;
-			cout << "Matrix C =" << endl << C;
+			cout << "Matrix B =" << endl
+				 << B;
+			cout << "Matrix C =" << endl
+				 << C;
 
 			C = B;
 
-			cout << "Matrix C =" << endl << C;
+			cout << "Matrix C =" << endl
+				 << C;
 
 			BbMatrixD AA(3, 3, 1.);
-			AA(1,1) = 1.;
-			AA(1,2) = 2.;
-			AA(1,3) = 3.;
+			AA(1, 1) = 1.;
+			AA(1, 2) = 2.;
+			AA(1, 3) = 3.;
 			BbVectorD bb(3);
 			bb(1) = 4.;
 			bb(2) = 3.;
@@ -171,36 +171,34 @@ int main(int argc, char* argv[])
 		}
 
 		{
-			BbMatrixD R
-			{
-				{ 1, 2, 3 },
-				{ -1, 0, 1 },
-				{ 3, 1, 3 }
-			};
-			BbVectorD b{ -5, -3, -3 };
+			BbMatrixD R{
+				{1, 2, 3},
+				{-1, 0, 1},
+				{3, 1, 3}};
+			BbVectorD b{-5, -3, -3};
 
-	//		BbMatrix R
-	//		{
-	//			{ 10., 1.E+20 },
-	//			{ 1., -.9 }
-	//		};
-	//		BbVector b{ 1.E+20, .1 };
+			//		BbMatrix R
+			//		{
+			//			{ 10., 1.E+20 },
+			//			{ 1., -.9 }
+			//		};
+			//		BbVector b{ 1.E+20, .1 };
 
-	//		BbMatrix R
-	//		{
-	//			{ 10., 1., 0. },
-	//			{ .5, 1.E-20, 1. },
-	//			{ 0., 0., 1. }
-	//		};
-	//		BbVector b{ 1.E+20, 1., 1. };
+			//		BbMatrix R
+			//		{
+			//			{ 10., 1., 0. },
+			//			{ .5, 1.E-20, 1. },
+			//			{ 0., 0., 1. }
+			//		};
+			//		BbVector b{ 1.E+20, 1., 1. };
 
-	//		BbMatrix R
-	//		{
-	//			{ -1., 1., -1. },
-	//			{ 10., -5., 10. },
-	//			{ 2., 1., 1. }
-	//		};
-	//		BbVector b{ 1., -3., 2. };
+			//		BbMatrix R
+			//		{
+			//			{ -1., 1., -1. },
+			//			{ 10., -5., 10. },
+			//			{ 2., 1., 1. }
+			//		};
+			//		BbVector b{ 1., -3., 2. };
 
 			cout << "Gauss elimination:" << endl;
 
@@ -224,16 +222,13 @@ int main(int argc, char* argv[])
 		{
 			cout << "Push back row: " << endl;
 
-			BbMatrixD R
-			{
-				{ 1, 2, 3 },
-				{ -1, 0, 1 },
-				{ 3, 1, 3 }
-			};
+			BbMatrixD R{
+				{1, 2, 3},
+				{-1, 0, 1},
+				{3, 1, 3}};
 
 			sw.restart();
-			for (int i = 1; i <= 5000000; ++i)
-			{
+			for (int i = 1; i <= 5000000; ++i) {
 				R.push_back_row(R.get_row(1));
 				R.chop_row();
 			}
@@ -242,8 +237,7 @@ int main(int argc, char* argv[])
 			cout << "Push back column: " << endl;
 
 			sw.restart();
-			for (int i = 1; i <= 5000000; ++i)
-			{
+			for (int i = 1; i <= 5000000; ++i) {
 				R.push_back_column(R.get_row(1));
 				R.chop_column();
 			}
@@ -254,56 +248,66 @@ int main(int argc, char* argv[])
 			cout << "Insert row:" << endl;
 			BbMatrixD AAA(5, 3, 2.);
 			BbVectorD bbb(3, 3.);
-			cout << "AAA before row insertion: " << endl << AAA << endl;
+			cout << "AAA before row insertion: " << endl
+				 << AAA << endl;
 			AAA.insert_row(3, bbb);
 			AAA.push_back_row(bbb);
-			cout << "AAA after row insertion: " << endl << AAA << endl;
+			cout << "AAA after row insertion: " << endl
+				 << AAA << endl;
 
 			cout << "Insert column:" << endl;
 			BbVectorD bbbb(7, 4.);
-			cout << "AAA before column insertion: " << endl << AAA << endl;
+			cout << "AAA before column insertion: " << endl
+				 << AAA << endl;
 			AAA.insert_column(2, bbbb);
 			AAA.push_back_column(bbbb);
-			cout << "AAA after column insertion: " << endl << AAA << endl;
+			cout << "AAA after column insertion: " << endl
+				 << AAA << endl;
 
 			cout << "Delete row:" << endl;
-			cout << "AAA before row deletion: " << endl << AAA << endl;
+			cout << "AAA before row deletion: " << endl
+				 << AAA << endl;
 			AAA.delete_row(1);
 			AAA.chop_row();
-			cout << "AAA after row deletion: " << endl << AAA << endl;
+			cout << "AAA after row deletion: " << endl
+				 << AAA << endl;
 
 			cout << "Delete column:" << endl;
-			cout << "AAA before column deletion: " << endl << AAA << endl;
+			cout << "AAA before column deletion: " << endl
+				 << AAA << endl;
 			AAA.delete_column(2);
 			AAA.chop_column();
-			cout << "AAA after column deletion: " << endl << AAA << endl;
+			cout << "AAA after column deletion: " << endl
+				 << AAA << endl;
 
 			cout << "Swap rows:" << endl;
 
 			sw.restart();
-			for (int i = 0; i < 100000000; ++i)
-			{
+			for (int i = 0; i < 100000000; ++i) {
 				AAA.swap_rows(2, 3);
 			}
-			cout << "AAA after row swap: " << endl << AAA << endl << "Took " << sw.split() << " ms" << endl;
-			AAA.push_back_column({ 666., 666., 666., 666., 666. });
-			cout << "AAA after addition of a column: " << endl << AAA << endl;
+			cout << "AAA after row swap: " << endl
+				 << AAA << endl
+				 << "Took " << sw.split() << " ms" << endl;
+			AAA.push_back_column({666., 666., 666., 666., 666.});
+			cout << "AAA after addition of a column: " << endl
+				 << AAA << endl;
 
 			cout << "Swap columns:" << endl;
 			sw.restart();
-			for (int i = 0; i < 100000000; ++i)
-			{
+			for (int i = 0; i < 100000000; ++i) {
 				AAA.swap_columns(1, 4);
 			}
-			cout << "AAA after column swap: " << endl << AAA << endl << "Took " << sw.split() << " ms" << endl;
+			cout << "AAA after column swap: " << endl
+				 << AAA << endl
+				 << "Took " << sw.split() << " ms" << endl;
 		}
 
 		{
 			BbMatrixD A(10, 10, 666.);
 
 			sw.restart();
-			for (int i = 1; i <= 1000000; ++i)
-			{
+			for (int i = 1; i <= 1000000; ++i) {
 				A.transpose();
 				if (i < 5)
 					cout << A << endl;
@@ -313,8 +317,7 @@ int main(int argc, char* argv[])
 
 		{
 			sw.restart();
-			for (int i = 1; i <= 10000000; ++i)
-			{
+			for (int i = 1; i <= 10000000; ++i) {
 				BbMatrixD A(10, 10, 666.);
 				A[6][6] = 667.;
 			}
@@ -326,8 +329,7 @@ int main(int argc, char* argv[])
 			BbMatrixD* A;
 
 			sw.restart();
-			for (int i = 1; i <= 10000000; ++i)
-			{
+			for (int i = 1; i <= 10000000; ++i) {
 				A = new BbMatrixD(10, 10, 666.);
 				(*A)[6][6] = 667.;
 				delete A;
@@ -335,7 +337,7 @@ int main(int argc, char* argv[])
 
 			cout << "Created and deleted a 10x10 matrix for 10M times in " << sw.split() << " ms" << endl;
 		}
-	/*
+		/*
 		{
 			// Test iterators
 			std::default_random_engine generator;
@@ -404,51 +406,45 @@ int main(int argc, char* argv[])
 			BbVectorD v(5, 3.);
 			BbVectorD wrong(4);
 
-			cout << "A = " << endl << A << endl;
+			cout << "A = " << endl
+				 << A << endl;
 
 			sw.restart();
 
-			for (int i = 1; i < 1E+7; ++i)
-			{
+			for (int i = 1; i < 1E+7; ++i) {
 				A.set_row(3, v);
 			}
 			cout << "set_row() with BbVector took " << sw.split() << " ms" << endl;
 
-			for (int i = 1; i < 1E+7; ++i)
-			{
+			for (int i = 1; i < 1E+7; ++i) {
 				A.set_row(2, 5.);
 			}
 			cout << "set_row() with double took " << sw.split() << " ms" << endl;
 
-			for (int i = 1; i < 1E+7; ++i)
-			{
+			for (int i = 1; i < 1E+7; ++i) {
 				A.set_column(3, v);
 			}
 			cout << "set_column() with BbVector took " << sw.split() << " ms" << endl;
 
-			for (int i = 1; i < 1E+7; ++i)
-			{
+			for (int i = 1; i < 1E+7; ++i) {
 				A.set_column(4, 7.);
 			}
 			cout << "set_column() with double took " << sw.split() << " ms" << endl;
 
-			for (int i = 1; i < 1E+7; ++i)
-			{
-				A.set_diagonal({ 0., 0., 0., 0., 0. });
+			for (int i = 1; i < 1E+7; ++i) {
+				A.set_diagonal({0., 0., 0., 0., 0.});
 			}
 			cout << "set_diagonal() with initializer_list took " << sw.split() << " ms" << endl;
 
-			for (int i = 1; i < 1E+7; ++i)
-			{
+			for (int i = 1; i < 1E+7; ++i) {
 				A.set_diagonal(v);
 			}
 			cout << "set_diagonal() with BbVector took " << sw.split() << " ms" << endl;
 
-			cout << "A = " << endl << A << endl;
+			cout << "A = " << endl
+				 << A << endl;
 		}
-	}
-	catch (const exception& ex)
-	{
+	} catch (const exception& ex) {
 		cout << "Error! Message: " << ex.what() << endl;
 	}
 
